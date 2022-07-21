5f754455f156f19a15a6dacb4bd486ff0420cb8e
Shupp <erikk.shupp@gmail.com>
2022-07-20 21:10:27 -0400

    renamed testfile and testing the PyPDF2 read ability.

--- python.py
+++ python.py
@@ -1,8 +1,22 @@
-import PyPDF2
-filename= input("Enter file name:")
-pdfFileObj = open(filename,'rb')
-pdfReader = pyPDF2.PdfFileReader(pdfFileObj)
-pdfReader.numPages
+# extract_doc_info.py
+from PyPDF2 import PdfFileReader
+pdf_path = "testfile.pdf"
 
-pageObj = pdfReader.getpage(0)
-pageObj.extractText()
+page=0
+
+def extract_information(pdf_path):
+    with open(pdf_path, "rb") as f:
+        pdf = PdfFileReader(f)
+        print(pdf.numPages)
+        currentpage = pdf.getPage(page)
+        pdfData=currentpage.extractText()
+        print(pdfData)
+
+for x in page:
+    print(pdfData)
+    page + 1
+
+path = pdf_path
+extract_information(path)
+
+
